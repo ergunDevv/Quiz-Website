928c81c8e96805a9b3d7546657a0c6d1429d8a1a
ergunDevv <ergncyln2127088@gmail.com>
2022-11-04 11:11:11 +0300

    Route basic system added , components will add soon

--- src/App.vue
+++ src/App.vue
@@ -2,44 +2,38 @@
   <div class="container">
     <header>
       <h1>Quizes</h1>
-      <input v-model.trim="search" type="text" placeholder="Search...">
-
+      <input v-model.trim="search" type="text" placeholder="Search..." />
     </header>
     <div class="options-container">
-      <div v-for="quiz in quizes" :key="quiz.id" class="card">
-          <img :src="quiz.img" alt="">
-          <div class="card-text">
-            <h2>{{ quiz.name }}</h2>
-            <p>{{quiz.questions.length}} questions</p>
-          </div>
-      </div>
+      <CardComponent v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref , watch } from 'vue';
-import qData from './data/quizes.json'
+import { ref, watch } from "vue";
+import qData from "./data/quizes.json";
+import CardComponent from "./components/CardComponent.vue";
 
 const quizes = ref(qData);
 const search = ref("");
 
-watch(search,  ()=>{
-  quizes.value = qData.filter(quiz=>quiz.name.toLowerCase().includes(search.value.toLowerCase()))
-})
-
+watch(search, () => {
+  quizes.value = qData.filter((quiz) =>
+    quiz.name.toLowerCase().includes(search.value.toLowerCase())
+  );
+});
 </script>
 
 
 <style scoped>
-
-.container{
+.container {
   max-width: 1000px;
   margin: 0 auto;
 }
-header{
+header {
   margin-bottom: 10px;
-  margin-top:10px;
+  margin-top: 10px;
   display: flex;
   align-items: center;
 }
@@ -47,43 +41,15 @@ header h1 {
   font-weight: bold;
   margin-right: 25px;
 }
-header input{
-  border:none;
-  background-color: rgba(128,128,128,0.1);
+header input {
+  border: none;
+  background-color: rgba(128, 128, 128, 0.1);
   padding: 20px;
   border-radius: 5px;
-
 }
-.options-container{
+.options-container {
   display: flex;
   flex-wrap: wrap;
-  margin-top:40px
+  margin-top: 40px;
 }
-
-/* CARD */
-.card{
-  width:310px;
-  overflow: hidden;
-  border-radius: 2%;
-  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
-  margin-bottom: 35px;
-  margin-right: 20px;
-  cursor: pointer;
-}
-
-.card img {
-  width: 100%;
-  height: 190px;
-  margin:0;
-}
-
-.card .card-text {
-  padding :0 5px
-}
-
-.card .card-text {
-  font-weight: bold;
-}
-
-
 </style>
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,55 @@
+<script setup> 
+
+    import { defineProps } from 'vue';
+    import {RouterLink} from 'vue-router'
+    
+    const props = defineProps(['quiz'])
+
+</script>
+
+<template>
+<RouterLink :to="`/quiz/${props.quiz.id}`">
+
+  <div>
+      <div class="card">
+          <img :src="props.quiz.img" alt="">
+          <div class="card-text">
+            <h2>{{ props.quiz.name }}</h2>
+            <p>{{props.quiz.questions.length}} questions</p>
+          </div>
+      </div>
+  </div>
+</RouterLink>
+</template>
+
+
+<style>
+
+
+/* CARD */
+.card{
+    width:310px;
+    overflow: hidden;
+    border-radius: 2%;
+    box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+    margin-bottom: 35px;
+    margin-right: 20px;
+    cursor: pointer;
+    text-decoration: none;
+  }
+  
+  .card img {
+    width: 100%;
+    height: 190px;
+    margin:0;
+  }
+  
+  .card .card-text {
+    padding :0 5px;
+    text-decoration: none;
+  }
+  
+  .card .card-text {
+    font-weight: bold;
+  }
+  </style>
