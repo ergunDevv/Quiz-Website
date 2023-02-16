78ebb398dc7a0efc52e0dbd93313fdaaf55c3e73
ergunDevv <ergncyln2127088@gmail.com>
2023-02-16 13:15:50 +0300

    Every component connected, Data base expanded more questions, ui changes

--- src/components/ResultComponent.vue
+++ src/components/ResultComponent.vue
@@ -1,10 +1,15 @@
 <script setup>
+import { defineProps } from 'vue';
+import { RouterLink } from 'vue-router';
+const props = defineProps(["quizQuestionLength" , "numberOfCorrectAnswers"])
+
 </script>
 
 <template>
   <div class="results">
     <p>Your Results...</p>
-    <h1>4/8</h1>
+    <h1>{{props.numberOfCorrectAnswers}} / {{quizQuestionLength}}</h1>
+    <RouterLink to="/"><h3>Go to Home</h3></RouterLink>
   </div>
 </template>
 
